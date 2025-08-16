102b5995643802b821112e517870a67bf2a461a4
Evo-Pro <7450709-Evo-Pro@users.noreply.replit.com>
2025-08-16 08:34:23 +0000

    Restored to 'f644f0d9928b55684a61b31c8cec597edf5692f1'
    Replit-Restored-To: f644f0d9928b55684a61b31c8cec597edf5692f1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,30 @@
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Main } from "../js/main";
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+import { Main } from "@/pages/Main";
+
+function Router() {
+  return (
+    <Switch>
+      {/* Add pages below */}
+      <Route path="/" component={Main} />
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Main />
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
